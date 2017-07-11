ce131b451829c15613d8770631634ccaa101417d
maartenbreddels <maartenbreddels@gmail.com>
2017-07-11 22:54:05 +0200

    cleanups

--- ipysheet/sheet.py
+++ ipysheet/sheet.py
@@ -4,9 +4,7 @@ from traitlets import Unicode, CFloat, CInt, List, Instance, Union, Dict, Bool
 
 @widgets.register('ipysheet.Cell')
 class Cell(widgets.Widget):
-    #_view_name = Unicode('CellView').tag(sync=True)
     _model_name = Unicode('CellModel').tag(sync=True)
-    #_view_module = Unicode('ipysheet').tag(sync=True)
     _model_module = Unicode('ipysheet').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
@@ -25,7 +23,6 @@ class Sheet(widgets.DOMWidget):
     _model_module = Unicode('ipysheet').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    value = Unicode('Sheet World!').tag(sync=True)
     rows =  CInt(3).tag(sync=True)
     columns =  CInt(4).tag(sync=True)
     data = List(Instance(list), [[]]).tag(sync=True)
