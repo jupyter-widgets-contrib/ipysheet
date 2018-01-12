1ece612a47c0a9edd04e5d3fd551ba6202cfb8c6
maartenbreddels <maartenbreddels@gmail.com>
2018-01-12 10:06:26 +0100

    fix: removed pikaday css require, is already included in handsontable.full.css

--- js/src/handsontable_fix.js
+++ js/src/handsontable_fix.js
@@ -2,7 +2,6 @@ var Handsontable = require('handsontable/dist/handsontable.full.js')
 //window.Handsontable = Handsontable // TODO: fix this upstream, one of the handsontable renderers needs this
 require('style!css!handsontable/dist/handsontable.min.css')
 require('style!css!./custom.css')
-require('style!css!../_/pikaday/pikaday.css')
 
 module.exports = Handsontable
 
