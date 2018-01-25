7cc408ceceb9d17522761939e7d862f59c4c9774
Maarten Breddels <maartenbreddels@gmail.com>
2018-01-25 20:05:57 +0100

    fix #8: value->default_value and Type -> Type() for traits

--- ipysheet/sheet.py
+++ ipysheet/sheet.py
@@ -8,7 +8,7 @@ class Cell(widgets.Widget):
     _model_module = Unicode('ipysheet').tag(sync=True)
     #_view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    value = Union([Bool(), Unicode(), Float(), Int()], allow_none=True, value=None).tag(sync=True)
+    value = Union([Bool(), Unicode(), Float(), Int()], allow_none=True, default_value=None).tag(sync=True)
     row =  CInt(3).tag(sync=True)
     column =  CInt(4).tag(sync=True)
     type = Unicode('text').tag(sync=True)
@@ -16,7 +16,7 @@ class Cell(widgets.Widget):
     style = Dict({}).tag(sync=True)
     renderer = Unicode(None, allow_none=True).tag(sync=True)
     read_only = Bool(False).tag(sync=True)
-    choice = List(Unicode, allow_none=True, default_value=None).tag(sync=True)
+    choice = List(Unicode(), allow_none=True, default_value=None).tag(sync=True)
     format = Unicode('0.[000]', allow_none=True).tag(sync=True)
 
 # Bug in traitlets, it doesn't set it, which triggers the bug fixed here:
@@ -26,7 +26,7 @@ Cell.choice.default_value = None
 
 @widgets.register('ipysheet.Range')
 class Range(widgets.Widget):
-    value = Union([List(), List(Instance(list))], value=[0, 1]).tag(sync=True)
+    value = Union([List(), List(Instance(list))], default_value=[0, 1]).tag(sync=True)
 
 @widgets.register('ipysheet.Sheet')
 class Sheet(widgets.DOMWidget):
@@ -42,10 +42,10 @@ class Sheet(widgets.DOMWidget):
     data = List(Instance(list), [[]]).tag(sync=True)
     cells = Tuple().tag(sync=True, **widgets.widget_serialization)
     named_cells = Dict(value={}, allow_none=False).tag(sync=True, **widgets.widget_serialization)
-    row_headers = Union([Bool(), List(Unicode)], value=True).tag(sync=True)
-    column_headers = Union([Bool(), List(Unicode)], value=True).tag(sync=True)
+    row_headers = Union([Bool(), List(Unicode())], default_value=True).tag(sync=True)
+    column_headers = Union([Bool(), List(Unicode())], default_value=True).tag(sync=True)
     stretch_headers = Unicode('all').tag(sync=True)
-    column_width = Union([CInt(), List(CInt)], value=None, allow_none=True).tag(sync=True)
+    column_width = Union([CInt(), List(CInt())], default_value=None, allow_none=True).tag(sync=True)
 
     def __getitem__(self, item):
         '''Gets a previously created cell at row and column
