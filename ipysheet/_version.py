8bd425d79f6a1937dae2b3b1d81c602beca3e1a5
Maarten Breddels <maartenbreddels@gmail.com>
2018-01-30 12:35:11 +0100

    Release 0.1.0-rc.2

--- ipysheet/_version.py
+++ ipysheet/_version.py
@@ -1,6 +1,6 @@
 __version_tuple__ = (0, 1, 0, 'rc.1')
-__version_tuple_js__ = (0, 1, 0, 'rc.1')
+__version_tuple_js__ = (0, 1, 0, 'rc.2')
 __version__ = '0.1.0-rc.1'
-__version_js__ = '0.1.0-rc.1'
+__version_js__ = '0.1.0-rc.2'
 
 version_info = __version_tuple__ # kept for backward compatibility
