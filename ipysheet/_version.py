9a024653e8c0eff2dfabef10f45e1c361b3efa8a
Maarten Breddels <maartenbreddels@gmail.com>
2018-05-02 20:28:41 +0200

    Release 0.2.0 of py

--- ipysheet/_version.py
+++ ipysheet/_version.py
@@ -1,6 +1,6 @@
-__version_tuple__ = (0, 2, 0, 'beta.2')
+__version_tuple__ = (0, 2, 0)
 __version_tuple_js__ = (0, 2, 0, 'beta.2')
-__version__ = '0.2.0-beta.2'
+__version__ = '0.2.0'
 __version_js__ = '0.2.0-beta.2'
 
 version_info = __version_tuple__ # kept for backward compatibility
