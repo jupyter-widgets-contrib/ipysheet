88a64661fc0be24202f36ec13eb40b33ab21b892
martinRenou <martin.renou@gmail.com>
2019-04-10 20:45:32 +0200

    Release 0.3.4

--- ipysheet/_version.py
+++ ipysheet/_version.py
@@ -1,6 +1,6 @@
-__version_tuple__ = (0, 3, 3)
-__version_tuple_js__ = (0, 3, 4)
-__version__ = '0.3.3'
-__version_js__ = '0.3.4'
+__version_tuple__ = (0, 3, 4)
+__version_tuple_js__ = (0, 3, 5)
+__version__ = '0.3.4'
+__version_js__ = '0.3.5'
 
 version_info = __version_tuple__  # kept for backward compatibility
