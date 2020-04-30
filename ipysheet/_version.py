1e4564412f6e8791c565f4a3f069b8c08eb6304a
martinRenou <martin.renou@gmail.com>
2020-04-30 10:50:34 +0200

    Release 0.4.4
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- ipysheet/_version.py
+++ ipysheet/_version.py
@@ -1,6 +1,6 @@
-__version_tuple__ = (0, 4, 3)
-__version_tuple_js__ = (0, 4, 3)
-__version__ = '0.4.3'
-__version_js__ = '0.4.3'
+__version_tuple__ = (0, 4, 4)
+__version_tuple_js__ = (0, 4, 4)
+__version__ = '0.4.4'
+__version_js__ = '0.4.4'
 
 version_info = __version_tuple__  # kept for backward compatibility
